1afcf72c160d0d9a892726f2b68f9878a0b24f11
anthony <anthonycakins@gmail.com>
2024-01-03 19:32:46 +1000

    implemented mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+import { fetchData } from "./data/data"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Landing from "./components/Landing"
+
 export default function App() {
+const [a, sa] = useState([])
+
+  useEffect(() => {
+
+    async function t () {
+      const data = await fetchData()
+      console.log(data.results)
+      sa(data.results)
+    }
+    t()
+  }, [])
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+<Router>
+  <Routes>
+    <Route path="/" element={<Landing />}/>
+  </Routes>
+</Router>
   )
 }
